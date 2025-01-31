616aca880b47d6746da3acddb7494477bb2d1381
antogp24 <webdev32bits@gmail.com>
2025-01-30 22:55:15 -0500

    Solucionar code smell 6

--- model/ReservacionPaseo.java
+++ model/ReservacionPaseo.java
@@ -4,29 +4,31 @@ import composite.Component;
 import observer.NotificadorManager;
 
 public class ReservacionPaseo implements Component {
-    protected String tipo;
-    protected String estado;
+    protected Estado estado;
     protected String correoUsuario;
     protected double precio;
 
+    public static enum Estado {
+        DISPONIBLE, RESERVADA
+    }
+
     public ReservacionPaseo(String tipo, double precio) {
-        this.tipo = tipo;
         this.precio = precio;
-        this.estado = "disponible";
+        this.estado = Estado.DISPONIBLE;
     }
 
     @Override
     public void confirmar() {
-        if (estado.equals("disponible")) {
-            estado = "reservada";
+        if (estado == Estado.DISPONIBLE) {
+            estado = Estado.RESERVADA;
             NotificadorManager.getInstance().enviarNotificacion(this);
         }
     }
 
     @Override
     public void cancelar() {
-        if (estado.equals("reservada")) {
-            estado = "disponible";
+        if (estado == Estado.RESERVADA) {
+            estado = Estado.DISPONIBLE;
             NotificadorManager.getInstance().enviarNotificacion(this);
         }
     }
